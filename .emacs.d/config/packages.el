4db4ab047434b62e575e9192c7a061298b68ce36
podoth <podoth@gmail.com>
2013-05-28 19:43:06 +0900

    junkファイルの設定がおかしかったので修正
    Conflicts:
    
    	.emacs.d/config/packages.el

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -2,7 +2,7 @@
 ;;; auto-async-byte-compile
 ;;;
 (require 'auto-async-byte-compile)
-(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(setq auto-async-byte-compile-exclude-files-regexp "~/.junk/")
 (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
 
 ;;;
