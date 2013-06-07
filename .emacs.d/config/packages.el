99252c0a03eab21f4d59ae0256ec333c8d79c281
podoth <podoth@gmail.com>
2013-06-07 20:08:59 +0900

    flymake for linux kernel

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -912,3 +912,47 @@ PREFIX が t の場合 (前置引数がある場合) は、これまでの選択
 	  '(lambda () 
 	     (flymake-mode t)
 	     (define-key python-mode-map "\C-cd" 'credmp/flymake-display-err-minibuf)))
+
+;;;
+;;; Linux kernel向けflymake
+;;; 編集しているマシンでコンパイルするため、アーキテクチャの違いによるエラーの違いとかはでてしまう
+;;;
+(setq flymake-kernel-script "~/.emacs.d/etc/flymake-kernel.sh")
+(setq flymake-kernel-temporary-directory "~/.emacs.d/var/flymake-kernel")
+
+(make-variable-buffer-local 'flymake-kernel-source-dir)
+(make-variable-buffer-local 'flymake-kernel-build-dir)
+(make-variable-buffer-local 'flymake-kernel-config-file)
+(make-variable-buffer-local 'flymake-kernel-make-flags)
+
+(defun replace-slash-with-exclamations (s)
+  (replace-regexp-in-string "/" "!" s))
+
+(defun flymake-kernel-init (source-dir config-file  &optional make-flags)
+  (let* ((temp-file  (flymake-init-create-temp-buffer-copy
+		      'flymake-create-temp-inplace))
+	 (r-source-dir (expand-file-name source-dir))
+	 (r-config-file (expand-file-name config-file))
+	 (build-dir (concat (file-name-as-directory flymake-kernel-temporary-directory)
+			    (replace-slash-with-exclamations r-source-dir)))
+	 (r-build-dir (expand-file-name build-dir)))
+    (setq flymake-kernel-source-dir r-source-dir)
+    (setq flymake-kernel-build-dir r-build-dir)
+    (setq flymake-kernel-config-file r-config-file)
+    (setq flymake-kernel-config-file make-flags)
+    (if make-flags
+	(list flymake-kernel-script (list r-source-dir r-build-dir temp-file r-config-file make-flags))
+      (list flymake-kernel-script (list r-source-dir r-build-dir temp-file r-config-file)))
+))
+
+(defun flymake-kernel-rebuild ()
+  (interactive)
+    (call-process flymake-kernel-script nil nil nil flymake-kernel-source-dir flymake-kernel-build-dir "clean" flymake-kernel-config-file))
+
+(push '("/nfs/repo/orthros/[^o].*\\.c$" (lambda () (flymake-kernel-init
+					 "/nfs/repo/orthros"
+					 "~/.emacs.d/etc/flymake-kernel.config"
+					 "ARCH=x86_64 KCFLAGS+=\"-Wall\" KCFLAGS+=\"-Wextra\" KCFLAGS+=\"-Wshadow\"")))
+      flymake-allowed-file-name-masks)
+ ;; (setq flymake-log-level 3)
+
