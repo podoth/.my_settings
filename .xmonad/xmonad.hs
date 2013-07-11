ab9fa7e2530b15aa28a3e44c62ac0a32b846f583
podoth <podoth@gmail.com>
2013-07-11 13:00:45 +0900

    xmonadのレイアウト追加

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -11,6 +11,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Actions.GridSelect
 import XMonad.Config.Desktop (desktopLayoutModifiers)
 import XMonad.Layout.Grid
+import XMonad.Layout.Tabbed
 
 getWellKnownName :: Connection -> IO ()
 getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) -> getWellKnownName dbus)
@@ -24,20 +25,16 @@ getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) -> getWellKnow
 
 ------------------------------------------------------------------------
 -- --Layouts:
--- myLayout = tiled ||| Mirror tiled ||| Grid ||| Full
---   where
---      -- default tiling algorithm partitions the screen into two panes
---      tiled = Tall nmaster delta ratio
---      -- The default number of windows in the master pane
---      nmaster = 1
---      -- Default proportion of screen occupied by master pane
---      ratio = 1/2
---      -- Percent of screen to increment by when resizing panes
---      delta = 3/100
-
-
---  , layoutHook = myLayout
-
+myLayout = tiled ||| Mirror tiled ||| Grid ||| simpleTabbed ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled = Tall nmaster delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta = 3/100
 
 ------------------------------------------------------------------------
 
@@ -49,6 +46,7 @@ main = withConnection Session $ \ dbus -> do
   putStrLn "Got name, starting XMonad."
   xmonad $ gnomeConfig {
   manageHook = manageHook gnomeConfig
+  , layoutHook = desktopLayoutModifiers $ myLayout
   , logHook    = do
       logHook gnomeConfig
       dynamicLogWithPP $ defaultPP {
