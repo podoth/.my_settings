dc4318182269be6d990544383d00eb7a553b2537
podoth <podoth@gmail.com>
2013-07-11 19:45:21 +0900

    helmの機能追加

--- .emacs.d/config/helm-common.el
+++ .emacs.d/config/helm-common.el
@@ -26,6 +26,8 @@
      (define-key helm-map (kbd "C-w") 'backward-kill-word)
      (define-key helm-map (kbd "TAB") 'helm-next-line)
      (define-key helm-map (kbd "C-l") 'helm-recenter-top-bottom-other-window)
+     (define-key helm-map (kbd "C-S-n") (lambda () (interactive) (helm-next-line)(helm-next-line)(helm-next-line)))
+     (define-key helm-map (kbd "C-S-p") (lambda () (interactive) (helm-previous-line)(helm-previous-line)(helm-previous-line)))
      ))
 ;; 自動補完を無効
 (custom-set-variables '(helm-ff-auto-update-initial-value nil))
@@ -37,8 +39,17 @@
 ;;;
 ;;; helm-ag
 ;;;
-(autoload 'helm-ag "helm-ag")
-(autoload 'helm-ag-this-file "helm-ag")
+(autoload 'helm-ag "helm-ag" nil t)
+(autoload 'helm-ag-this-file "helm-ag" nil t)
 (global-set-key (kbd "C-c /") 'helm-ag)
 ;; (global-set-key (kbd "C-M-s") 'helm-ag-this-file)
 
+;;;
+;;; helm-descbinds
+;;;
+(autoload 'helm-descbinds "helm-descbinds" nil t)
+(defalias 'helm-describe-bindings 'helm-descbinds)
+(eval-after-load 'helm-descbinds
+  '(progn
+     (setq helm-descbinds-window-style 'split-window)
+     ))
