5e009f3845a61a7e194e9c2bf0c115d60a898e2b
podoth <podoth@gmail.com>
2014-01-13 10:51:13 +0900

    auto-complete-rubyがうまく動かない

--- .emacs.d/packages/auto-complete-ruby.el
+++ .emacs.d/packages/auto-complete-ruby.el
@@ -4,19 +4,19 @@
 (defvar ac-ruby-sources
   '(ac-source-rcodetools))
 
-(defvar ac-source-rcodetools
-  '((init . (lambda ()
-              (condition-case x
-                  (save-excursion
-                    (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles"))
-                (error) (setq rct-method-completion-table nil))))
-    (candidates . (lambda ()
-                    (all-completions
-                     ac-prefix
-                     (mapcar
-                      (lambda (completion)
-                        (replace-regexp-in-string "\t.*$" "" (car completion)))
-                      rct-method-completion-table))))))
+;; (defvar ac-source-rcodetools
+;;   '((init . (lambda ()
+;;               (condition-case x
+;;                   (save-excursion
+;;                     (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles"))
+;;                 (error) (setq rct-method-completion-table nil))))
+;;     (candidates . (lambda ()
+;;                     (all-completions
+;;                      ac-prefix
+;;                      (mapcar
+;;                       (lambda (completion)
+;;                         (replace-regexp-in-string "\t.*$" "" (car completion)))
+;;                       rct-method-completion-table))))))
 
 (defun ac-ruby-setup ()
   (setq ac-sources (append ac-sources ac-ruby-sources))
